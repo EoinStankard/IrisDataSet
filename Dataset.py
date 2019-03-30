cc9a74c799dd474fb890d7a005f3ad9db23b068e
Eoin Stankard <G00300390@gmit.ie>
2019-03-30 21:14:42 +0000

    Added functions for Max,Min and Average, No Reading of CSV file yet

--- Dataset.py
+++ Dataset.py
@@ -9,11 +9,48 @@ print("Please Select Iris Flower")
 print("1. Setosa\n2. Versicolor\n3. Virginica\n4. Exit")
 choice = input("Choice: ")
 
+def maxNum(c):
+    #Calculate max here
+    if c=="s":
+        print("Sepal Max")
+    elif c =="p":
+        print("Petal Max")
+
+def minNum(c):
+    if c=="s":
+        print("Sepal Min")
+    elif c =="p":
+        print("Petal Min")
+
+def avgNum(c):
+    if c=="s":
+        print("Sepal Average")
+    elif c =="p":
+        print("Petal Average")
+
+
+
 def flowerFunct(F):
     os.system('cls')
     print(f"Options for {F}")
     print("1. Max Sepal Length\n2. Min Sepal Length\n3. Avg Sepal Length")
     print("4. Max Petal Length\n5. Min Petal Length\n6. Avg Petal Length")
+    option = input("Choice: ")
+
+    if int(option)==1:
+        maxNum("s")
+    elif int(option)==2:
+        minNum("s")
+    elif int(option)==3:
+        avgNum("s")
+    elif int(option)==4:
+        maxNum("p")
+    elif int(option)==5:
+        minNum("p")
+    elif int(option)==6:
+        avgNum("p")
+    else:
+        print("Error")
 
 
 
